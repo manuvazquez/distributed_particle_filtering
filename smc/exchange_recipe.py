7ee22039e89dbe720cbc023dc68bbf516f30d2af
Manuel A. Vázquez <manuavazquez@gmail.com>
2016-05-24 18:30:23 +0200

    - smc 	- exchange_recipe.py 		- ExchangeRecipe 			- __init__ 				- fixed bug 					- "self._network_radius" -> "self._network_diameter"... => 					- ...the diameter is computed/used rather than the radius

--- smc/exchange_recipe.py
+++ smc/exchange_recipe.py
@@ -28,9 +28,7 @@ class ExchangeRecipe(metaclass=abc.ABCMeta):
 		self._n_PEs = processing_elements_topology.n_processing_elements
 
 		# the "radius" of the network is half the maximum number of hops between any two PEs
-		self._network_radius = np.ceil(
-			processing_elements_topology.distances_between_processing_elements.max() / 2
-		).astype(int)
+		self._network_diameter = processing_elements_topology.distances_between_processing_elements.max()
 
 	def randomized_wakeup(self, n, PRNG):
 
@@ -749,9 +747,9 @@ class SetMembershipConstrainedExchangeRecipe(ExchangeRecipe):
 		self._n_particles_per_PE = n_particles_per_PE
 		self._PRNG = PRNG
 
-		self._n_iterations_global_set_determination = self._network_radius
+		self._n_iterations_global_set_determination = self._network_diameter
 		self._n_iterations_likelihood_consensus = ad_hoc_parameters["iterations for likelihood consensus"]
-		self._n_iterations_max_min_likelihood_consensus = self._network_radius
+		self._n_iterations_max_min_likelihood_consensus = self._network_diameter
 		self._mu = ad_hoc_parameters["mu for likelihood consensus"]
 
 		# a list of lists in which each element yields the neighbors of a PE
@@ -842,7 +840,7 @@ class SelectiveGossipExchangeRecipe(ExchangeRecipe):
 
 		self._n_iterations_selective_gossip = ad_hoc_parameters["iterations for selective gossip"]
 		self._n_components_selective_gossip = ad_hoc_parameters["number of significant components for selective gossip"]
-		self._n_iterations_max_gossip = self._network_radius
+		self._n_iterations_max_gossip = self._network_diameter
 
 		# a list of lists in which each element yields the neighbors of a PE
 		self._neighborhoods = processing_elements_topology.get_neighbours()
