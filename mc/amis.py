- mc 	- added "plot_log_normal.py" - fix bug: the path loss exponent must be a positive random variable: also log-normal 	- util.py 		- loglikelihood 			- "np.exp" also applied for the path loss exponent 	- pmc.py, amis.py 		- adjusted_mean is the exponential for every parameter 	- simulation 		- npmc 			- AbstractNPMC 				- __init__ 					- "path loss exponent" is also log-normal

mc/amis.py
@@ -123,8 +123,7 @@ class AdaptiveMultipleImportanceSampling(pmc.PopulationMonteCarlo):
 		# self.update_proposal()
 		self._proposal_updater.update_proposal(self)
 
-		adjusted_mean = self._mean.copy()
-		adjusted_mean[:2] = np.exp(adjusted_mean[:2])
+		adjusted_mean = np.exp(self._mean.copy())
 
 		# the number of iteration is increased
 		self._i_iter += 1

mc/pmc.py
@@ -87,8 +87,7 @@ class PopulationMonteCarlo(smc.particle_filter.particle_filter.ParticleFilter):
 		# self.update_proposal()
 		self._proposal_updater.update_proposal(self)
 
-		adjusted_mean = self._mean.copy()
-		adjusted_mean[:2] = np.exp(adjusted_mean[:2])
+		adjusted_mean = np.exp(self._mean.copy())
 
 		print('mean:\n', self._mean)
 		print('covar:\n', self._covar)

mc/util.py
@@ -27,7 +27,7 @@ def loglikelihood(pf, observations, log_tx_power, log_min_power, path_loss_exp):
 
 		s.set_parameters(
 			tx_power=np.exp(log_tx_power), minimum_amount_of_power=np.exp(log_min_power),
-			path_loss_exponent=path_loss_exp)
+			path_loss_exponent=np.exp(path_loss_exp))
 
 	# this is needed to rebuild the sensors with the new parameters
 	pf.reset_sensors_array()

simulations/npmc.py
@@ -67,21 +67,28 @@ class AbstractNPMC(simulations.base.SimpleSimulation, metaclass=abc.ABCMeta):
 		# ...and the parameters of the corresponding normal are
 		mean_min_power, var_min_power = mc.util.normal_parameters_from_lognormal(mean_log_min_power, var_log_min_power)
 
+		# ---
+
 		# the same for the *transmitter power*
 		mean_log_tx_power = self._simulation_parameters["prior"]["transmitter power"]["mean"]
 		var_log_tx_power = self._simulation_parameters["prior"]["transmitter power"]["variance"]
 
 		mean_tx_power, var_tx_power = mc.util.normal_parameters_from_lognormal(mean_log_tx_power, var_log_tx_power)
 
-		self._prior_mean = np.array([
-			mean_tx_power,
-			mean_min_power,
-			self._simulation_parameters["prior"]["path loss exponent"]["mean"]])
+		# ---
 
-		self._prior_covar = np.diag([
-			var_tx_power,
-			var_min_power,
-			self._simulation_parameters["prior"]["path loss exponent"]["variance"]])
+		# the same for the *transmitter power*
+		mean_log_path_loss_exponent = self._simulation_parameters["prior"]["path loss exponent"]["mean"]
+		var_log_path_loss_exponent = self._simulation_parameters["prior"]["path loss exponent"]["variance"]
+
+		mean_path_loss_exponent, var_path_loss_exponent = mc.util.normal_parameters_from_lognormal(
+			mean_log_path_loss_exponent, var_log_path_loss_exponent)
+
+		# ---
+
+		self._prior_mean = np.array([mean_tx_power, mean_min_power, mean_path_loss_exponent])
+
+		self._prior_covar = np.diag([var_tx_power, var_min_power, var_path_loss_exponent])
 
 		# ----- HDF5
 